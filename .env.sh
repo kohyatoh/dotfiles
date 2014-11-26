f5baa811e501725760915e700d3c1cae417a73e9
Kohsuke Yatoh <kohyatoh@gmail.com>
2014-11-26 18:15:16 +0900

    add $HERE

--- .env.sh
+++ .env.sh
@@ -2,7 +2,7 @@ set_ps1() {
     local g='\[\e[1;32m\]'
     local b='\[\e[1;34m\]'
     local n='\[\e[0m\]'
-    PS1="$g\t$n $b\w$n $b\$$n "
+    PS1="$g\t@$HERE$n $b\w$n $b\$$n "
 }
 set_ps1
 HISTTIMEFORMAT='%y/%m/%d %H:%M:%S '
